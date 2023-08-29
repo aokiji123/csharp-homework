d5deeed10ff50b6ac8b613da18ae8db3e8cd4e5b
mac <jenkaboy7@gmail.com>
2023-08-29 13:58:16 +0200

    homework

---
+++ 1-homework/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // 1)
+        Console.Write("Enter first number: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        double number3 = Convert.ToDouble(Console.ReadLine());
+
+        double average = (number1 + number2 + number3) / 3;
+
+        Console.WriteLine($"Average: {average}");
+
+        // 2)
+        Console.Write("Enter number: ");
+        double degreeNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter degree: ");
+        double degree = Convert.ToDouble(Console.ReadLine());
+
+        double result = Math.Pow(degreeNumber, degree);
+
+        Console.WriteLine($"Result: {result}");
+
+        // 3)
+        Console.Write("Enter number in UAH: ");
+        double moneyInUah = Convert.ToDouble(Console.ReadLine());
+
+        double moneyInUsd = moneyInUah * 0.027;
+        double moneyInEuro = moneyInUah * 0.025;
+
+        Console.WriteLine($"Money in USD: {moneyInUsd}");
+        Console.WriteLine($"Money in Euro: {moneyInEuro}");
+
+        // 4)
+        Console.Write("Enter number of km: ");
+        double km = Convert.ToDouble(Console.ReadLine());
+
+        double kmToMiles = km * 0.621371;
+        double kmToSeaMiles = km * 0.539957;
+
+        Console.WriteLine($"Km to Miles: {kmToMiles}");
+        Console.WriteLine($"Km to Sea Miles: {kmToSeaMiles}");
+
+        // 5)
+        Console.Write("Enter number: ");
+        double numberToFindPercent = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter percent: ");
+        double percent = Convert.ToDouble(Console.ReadLine());
+
+        double percentResult = numberToFindPercent * percent / 100;
+        Console.WriteLine($"Result: {percentResult}");
+
+        // 6)
+        Console.Write("What do you want to do (1 for celsius, 2 for fahrenheit: ");
+        double choice = Convert.ToDouble(Console.ReadLine());
+        if (choice == 1)
+        {
+            Console.Write("Enter celsius to convert them to fahrenheit: ");
+            double celsiusToFahrenheit = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsiusToFahrenheit * 9 / 5) + 32;
+            Console.WriteLine($"Result: {fahrenheit}");
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Enter fahrenheit to convert them to celsius: ");
+            double fahrenheitToCelsius = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheitToCelsius - 32) * 5 / 9;
+            Console.WriteLine($"Result: {celsius}");
+        }
+        else
+        {
+            throw new Exception("Error");
+        }
+
+    }
+}
---
+++ 2-homework/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static string[] bigNumbers = {
+        "  000  \n 0   0 \n0     0\n0     0\n 0   0 \n  000  ",
+        "   1   \n  11   \n   1   \n   1   \n   1   \n 11111 ",
+        "  222  \n 2   2 \n    2  \n   2   \n 2     \n 22222 ",
+        "  333  \n 3   3 \n     3 \n   33  \n     3 \n  333  ",
+        "    4  \n   44  \n  4 4  \n 4  4  \n 44444 \n    4  ",
+        " 55555 \n 5     \n 5555  \n     5 \n 5   5 \n  555  ",
+        "  666  \n 6     \n 6666  \n 6   6 \n 6   6 \n  666  ",
+        " 77777 \n     7 \n    7  \n   7   \n  7    \n 7     ",
+        "  888  \n 8   8 \n  888  \n 8   8 \n 8   8 \n  888  ",
+        "  9999 \n 9   9 \n  9999 \n     9 \n    9  \n  999  "
+    };
+
+    static void PrintBigNumber(string number)
+    {
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                int digitValue = int.Parse(digit.ToString());
+                Console.WriteLine(bigNumbers[digitValue]);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string inputNumber = Console.ReadLine();
+        PrintBigNumber(inputNumber);
+    }
+}
