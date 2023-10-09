2154dd8732db3d213a8071b23539408ae1daeb5a
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-10-09 13:46:50 +0200

    homework

---
+++ 4-homework/Program.cs
@@ -0,0 +1,85 @@
+﻿class Program
+{
+    delegate void MenuDelegate();
+
+    static void Main()
+    {
+        MenuDelegate menuItems = NewGame;
+        menuItems += LoadGame;
+        menuItems += ShowRules;
+        menuItems += ShowAuthor;
+        menuItems += Exit;
+
+        while (true)
+        {
+            DisplayMenu();
+
+            int choice = GetMenuChoice();
+
+            Delegate[] menuItemsList = menuItems.GetInvocationList();
+
+            if (choice > 0 && choice < menuItemsList.Length)
+            {
+                MenuDelegate option = (MenuDelegate)menuItemsList[choice - 1];
+
+                option.Invoke();
+            }
+            else if (choice == 0)
+                break;
+            else
+            {
+                Console.WriteLine("Недопустимый выбор. Попробуйте снова.");
+            }
+        }
+    }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Меню:");
+        Console.WriteLine("1 - Новая игра");
+        Console.WriteLine("2 - Загрузить игру");
+        Console.WriteLine("3 - Правила");
+        Console.WriteLine("4 - Об авторе");
+        Console.WriteLine("0 - Выход");
+    }
+
+    static int GetMenuChoice()
+    {
+        Console.Write("Выберите пункт меню: ");
+        string input = Console.ReadLine();
+        int choice;
+
+        if (!int.TryParse(input, out choice))
+        {
+            Console.WriteLine("Введите корректное число.");
+            return -1;
+        }
+
+        return choice;
+    }
+
+    static void NewGame()
+    {
+        Console.WriteLine("Начата новая игра.");
+    }
+
+    static void LoadGame()
+    {
+        Console.WriteLine("Игра загружена.");
+    }
+
+    static void ShowRules()
+    {
+        Console.WriteLine("Показываю правила игры.");
+    }
+
+    static void ShowAuthor()
+    {
+        Console.WriteLine("Информация об авторе.");
+    }
+
+    static void Exit()
+    {
+        Console.WriteLine("Выход из программы.");
+    }
+}
