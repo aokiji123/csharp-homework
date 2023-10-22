28dee3d2ac10100ee2db23157dd28b5d985fbaa8
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-10-22 12:59:22 +0200

    homework

---
+++ 5-homework/HashTable.cs
@@ -0,0 +1,75 @@
+﻿class HashTable<TKey, TValue>
+{
+    private const int Capacity = 100;
+
+    private List<KeyValuePair<TKey, TValue>>[] buckets;
+
+    public HashTable()
+    {
+        buckets = new List<KeyValuePair<TKey, TValue>>[Capacity];
+    }
+
+    private int GetHash(TKey key)
+    {
+        int hashCode = key.GetHashCode();
+        int index = Math.Abs(hashCode % Capacity);
+        return index;
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        int index = GetHash(key);
+
+        if (buckets[index] == null)
+        {
+            buckets[index] = new List<KeyValuePair<TKey, TValue>>();
+        }
+
+        foreach (var bucket in buckets[index])
+        {
+            if (bucket.Key.Equals(key))
+            {
+                throw new InvalidOperationException("Key already exists");
+            }
+        }
+
+        buckets[index].Add(new KeyValuePair<TKey, TValue>(key, value));
+    }
+
+    public TValue Get(TKey key)
+    {
+        int index = GetHash(key);
+
+        if (buckets[index] != null)
+        {
+            foreach (var bucket in buckets[index])
+            {
+                if (bucket.Key.Equals(key))
+                {
+                    return bucket.Value;
+                }
+            }
+        }
+
+        throw new KeyNotFoundException("Key was not found in table");
+    }
+
+    public void Remove(TKey key)
+    {
+        int index = GetHash(key);
+
+        if (buckets[index] != null)
+        {
+            for (int i = 0; i < buckets[index].Count; i++)
+            {
+                if (buckets[index][i].Key.Equals(key))
+                {
+                    buckets[index].RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        throw new KeyNotFoundException("Key was not found in table");
+    }
+}
---
+++ 5-homework/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        HashTable<string, int> hashTable = new HashTable<string, int>();
+
+        hashTable.Add("one", 1);
+        hashTable.Add("two", 2);
+        hashTable.Add("three", 3);
+
+        Console.WriteLine("Key value of 'one': " + hashTable.Get("one"));
+        Console.WriteLine("Key value of 'two': " + hashTable.Get("two"));
+        Console.WriteLine("Key value of 'three': " + hashTable.Get("three"));
+
+        try
+        {
+            hashTable.Add("one", 10);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        hashTable.Remove("two");
+        Console.WriteLine("After deleting 'two':");
+        try
+        {
+            Console.WriteLine("Key value of 'two': " + hashTable.Get("two"));
+        }
+        catch (KeyNotFoundException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
