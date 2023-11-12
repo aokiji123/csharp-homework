5c6b80db3f10deaacfd3bb99919c94aa1fa4c119
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-11-12 17:30:45 +0100

    homework

---
+++ 6-homework/Program.cs
@@ -0,0 +1,25 @@
+﻿class CountFiles
+{
+    static void Main()
+    {
+        static int FilesCounting(string path)
+        {
+            int count = 0;
+            string[] files = Directory.GetFiles(path);
+            count += files.Length;
+
+            string[] subfiles = Directory.GetDirectories(path);
+            foreach (string subfile in subfiles)
+            {
+                count += FilesCounting(subfile);
+            }
+
+            return count;
+        }
+
+        string path = "/Users/mac/Desktop";
+        int fileCount = FilesCounting(path);
+
+        Console.WriteLine(fileCount);
+    }
+}
